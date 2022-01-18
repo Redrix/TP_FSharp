e5b4fa1c76698541a1c4f1fae5b723dd5c312fb4
Redrix <44963837+Redrix@users.noreply.github.com>
2022-01-18 11:17:26 +0100

    Final

--- TP_FSharp_Parot_Arnaud/Program.fs
+++ TP_FSharp_Parot_Arnaud/Program.fs
@@ -2,18 +2,15 @@
 open System.Text.RegularExpressions
 
 // Exo 1 : 
-(*
 let five = 5
 let seven = 7
 let cube x = x * x * x
 
 cube five |> printfn "%i"
 cube seven |> printfn "%i"
-*)
 
 //---------------------------------------------------------------------------------------------------------------
 
-(*
 let interestRate (balance: decimal): single = //implémenter la fonction
     match balance with
     | y when (y < 0m) -> 3.213f
@@ -27,21 +24,17 @@ let interest (balance: decimal): decimal = // implementer ici
 let annualBalanceUpdate(balance: decimal): decimal = // Implémenter
    balance + interest(balance)
 
-let amountToDonate(balance: decimal) (taxFreePercentage: float): int =
+//let amountToDonate(balance: decimal) (taxFreePercentage: float): int =
     
-
-// => 27
 
 interestRate 200.75m |> printfn "%f"
 interest 200.75m |> printfn "%f"
 annualBalanceUpdate 200.75m |> printfn "%f"
-*)
 
 //-----------------------------------------------------------------------------------------------------------------
 
 // Exo 2 :
 
-(*
 let message(chaine: string) : string =
     let section = chaine.Split[|':'|]
     section[1].Trim(' ')
@@ -75,12 +68,11 @@ talkToBOB "RGTHEJYJY" |> printfn "%s"
 talkToBOB "COMMENT CA VA ?" |> printfn "%s"
 talkToBOB "" |> printfn "%s"
 talkToBOB "Test" |> printfn "%s"
-*)
 
 //-------------------------------------------------------------------------------------------------------------------
 
 // Exo : 3
-(*
+
 type Coach = {Name: string; FormerPlayer: bool}
 type Stats = {Wins: int; Pertes: int}
 type Team = {Name: string; Coach: Coach; Stats: Stats}
@@ -123,13 +115,11 @@ let spursStats = createStats 56 26
 let spursTeam = createTeam "San Antonio Spurs" spursCoach spursStats
 rootForTeam spursTeam |> printfn "%b"
 
-(*
 let coach = createCoach "Larry Bird" true
 let record = createStats 58 24
 let newCoach = createCoach "Isiah Thomas" true
 let team = createTeam "Indiana Pacers" coach record
 replaceCoach team newCoach |> printfn "%A"
-*)
 
 // Partie : B
 
@@ -173,12 +163,11 @@ let printApprobation(approbation: Approbation): string =
     | Bof -> "Peut-être"
 
 printApprobation(rateActivity (Film Romance)) |> printfn "%s"
-*)
 
 //----------------------------------------------------------------------------------------------------
 
 // Exo 4 :
-(*
+
 let lastWeek = [|0; 2; 5; 3; 7; 8; 4|]
 
 let visitesHier(lastWeek: int[]): int =
@@ -194,21 +183,18 @@ let incrementTodaysCount(jours: int[]): int[] =
     jours[jours.Length - 1] <- jours[jours.Length - 1] + 1
     jours
 
-let oddWeek(week: int[]): bool =
-    let zero a = a = 0
-    week. |> Array.IndexOf zero week
-    match week with
-    | 
-    | _ -> false
+//let oddWeek(week: int[]): bool =
+        
+        
 
 visitesHier [|3; 5; 0; 7; 4; 1|] |> printfn "%i"
 total [|3; 5; 0; 7; 4; 1|] |> printfn "%i"
 joursSansVisite [|3; 5; 0; 7; 4; 1|] |> printfn "%b"
 let birdCount = [|3; 5; 0; 7; 4; 1|]
 incrementTodaysCount birdCount |> printfn "%A"
-oddWeek [| 1 ; 0 ; 5 ; 0 ; 12 ; 0 ; 2 |] |> printfn "%A"
-oddWeek [| 5 ; 0 ; 5 ; 12 ; 5 ; 3 ; 5|] |> printfn "%A"
-*)
+//oddWeek [|1; 0; 5; 0; 12; 0; 2|] |> printfn "%A"
+//oddWeek [|5; 0; 5; 12; 5; 3; 5|] |> printfn "%A"
+
 
 // Partie B :
 
